5c37c68b88ee62c26289baf47998335c85cad7f5
João Pires <joaopires51@gmail.com>
2012-04-10 17:38:16 +0100

    New Command.
    Signed-off-by: João Pires <joaopires51@gmail.com>

--- fooSpeech/Commands/CommandFactory.cs
+++ fooSpeech/Commands/CommandFactory.cs
@@ -7,7 +7,7 @@ namespace fooSpeech.Commands
 {
     static class CommandFactory
     {
-        private static Command[] commands = {new Play(), new Stop(), new VolumeDown(), new VolumeUp()};
+        private static Command[] commands = {new Play(), new Stop(), new VolumeDown(), new VolumeUp(), new NextMusic()};
 
 
         public static Command Contruct(String command)
---
+++ fooSpeech/Commands/NextMusic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace fooSpeech.Commands
+{
+    class NextMusic :Command
+    {
+        public NextMusic()
+            : base("Proxima Musica", new Uri("http://localhost:8888/ajquery/?cmd=StartNext&param3=NoResponse"))
+        {
+        }
+    }
+}
